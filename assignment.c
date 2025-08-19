e6665347287a2d23630ff4268275e5df32e8325c
DHYEYSHAH21 <dhyeyshah212007@gmail.com>
2025-08-19 17:10:26 +0530

    Create assignment.c

---
+++ assignment.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+
+int a,b,c,d;
+int main(){
+
+    printf("Enter Two Numbers:");
+    scanf("%d %d",&a,&b);
+    printf("Select which operation to perform (Only enter integral value from 1 to 4)\n1.Add\n2.Subtract\n3.Multiply\n4.Divide");
+    scanf("%d", &c);
+    switch (c)
+    {
+    case 1:
+        d=a+b;
+        printf("Your Output is: %d", d);
+        break;
+    case 2:
+        d=a-b;
+        printf("Your Output is: %d", d);
+        break;
+    case 3:
+        d=a*b;
+        printf("Your Output is: %d", d);
+        break;
+    case 4:
+        d=(a/b);
+        printf("Your Output is: %d", d);
+        break;
+    
+    default:
+    printf("Invalid output");
+        break;
+    }  
+
+}
